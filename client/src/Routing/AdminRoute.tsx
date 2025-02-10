e45e00b6d5d3a89a7ccad61cb422bbaa9b162b48
Suhailsubair007 <clashersuhail221@gmail.com>
2025-02-10 14:50:59 +0530

    App.tsx initailized and routing set

---
+++ client/src/Routing/AdminRoute.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AdminRoute: React.FC = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AdminRoute
---
+++ client/src/Routing/UserRoute.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserRoute: React.FC = () => {
+  return <div></div>;
+};
+
+export default UserRoute;
